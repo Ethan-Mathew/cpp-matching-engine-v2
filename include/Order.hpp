e00e38385f4b501bd04ee7d6012123b356d10f1a
Ethan Mathew <ethan.mathew55555@gmail.com>
2026-03-12 18:31:27 -0400

    feat: define Order type and constituent data fields

---
+++ include/Order.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Aliases.hpp"
+#include "Side.hpp"
+#include "TimeInForce.hpp"
+
+#include <cassert>
+
+struct alignas(64) Order
+{
+    Order* next_ = nullptr;
+    Order* prev_ = nullptr;
+    // PriceLevel* level_ = nullptr;
+    OrderID id_;
+    Price price_;
+    Quantity quantity_;
+    TimeInForce tif_;
+    Side side_;
+
+    Order() = delete;
+
+    Order(OrderID id, Price price, Quantity quantity, TimeInForce tif, Side side)
+        : id_{id}
+        , price_{price}
+        , quantity_{quantity}
+        , tif_{tif}
+        , side_{side}
+    {
+    }
+};
+
+static_assert(alignof(Order) == 64);
