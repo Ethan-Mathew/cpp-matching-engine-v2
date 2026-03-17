9ec71fdb71bb095938aa9577c0a9ce40b671aca8
Ethan Mathew <ethan.mathew55555@gmail.com>
2026-03-16 23:22:30 -0400

    fix: refactor order to accomodate incoming orders over a universal order type

---
+++ include/Types/IncomingOrder.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Aliases.hpp"
+#include "OrderType.hpp"
+#include "Side.hpp"
+#include "TimeInForce.hpp"
+
+struct IncomingOrder
+{
+    OrderID id_;
+    Price price_;
+    Quantity quantity_;
+    Side side_;
+    OrderType type_;
+    TimeInForce tif_;
+    
+    IncomingOrder() = delete;
+
+    IncomingOrder(OrderID id, Price price, Quantity quantity, Side side, OrderType type, TimeInForce tif)
+        : id_{id}
+        , price_{price}
+        , quantity_{quantity}
+        , side_{side}
+        , type_{type}
+        , tif_{tif}
+    {
+    }
+};
