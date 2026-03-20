3219e68a86173dd84884868c254f82c8efa8732a
Ethan Mathew <ethan.mathew55555@gmail.com>
2026-03-20 17:12:42 -0400

    feat: define basis for top-level OrderBook operations

--- include/lob/OrderBook.hpp
+++ include/lob/OrderBook.hpp
@@ -1,23 +1,37 @@
-#pragma once
-
 #include "Aliases.hpp"
+#include "OrderRequests.hpp"
 #include "PriceLevel.hpp"
 #include "RestingOrder.hpp"
+#include "UserSubmissionResults.hpp"
 
+#include <functional>
 #include <map>
+#include <optional>
 #include <unordered_map>
 
 namespace lob
 {
-    
+
 class OrderBook
 {
 public:
-    
+    OrderBook() = default;
+
+    void submit_limit_order();
+    void submit_market_order();
+    void modify_order();
+    void cancel_order();
+    void prune_good_for_day_orders();
+
 private:
-    std::map<Price, PriceLevel*, std::greater<>> bids_;
-    std::map<Price, PriceLevel*, std::less<>> asks_;
-    std::unordered_map<OrderID, RestingOrder*> allOrders_;
+    void submit_limit_order_fok();
+    void submit_limit_order_ioc();
+    void submit_limit_order_resting();
+  
+
+    std::unordered_map<OrderID, lob::core::RestingOrder*> idToOrderMap;
+    std::map<Price, lob::core::PriceLevel, std::greater<Price>> bidLevels_;
+    std::map<Price, lob::core::PriceLevel, std::less<Price>> askLevels_;
 };
 
-};
+} // namespace lob::core
