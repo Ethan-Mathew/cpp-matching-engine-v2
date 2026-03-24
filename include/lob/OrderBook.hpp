291e1d53f08348fc4a2bca34ca0fc76429a9678c
Ethan Mathew <ethan.mathew55555@gmail.com>
2026-03-24 18:21:00 -0400

    feat: add templated cross function to OrderBook API

--- include/lob/OrderBook.hpp
+++ include/lob/OrderBook.hpp
@@ -17,18 +17,21 @@ class OrderBook
 public:
     OrderBook() = default;
 
-    void submit_limit_order();
+    void submit_limit_order(const LimitOrderRequest& limitRequest);
     void submit_market_order();
     void modify_order();
     void cancel_order();
     void prune_good_for_day_orders();
 
 private:
+    template<Side S>
+    bool crosses(Price orderPrice, Price levelPrice) const;
+
     void submit_limit_order_fok();
     void submit_limit_order_ioc();
-    void submit_limit_order_resting();
+    template<Side S>
+    void submit_limit_order_resting(const LimitOrderRequest& limitRequest);
   
-
     std::unordered_map<OrderID, lob::core::RestingOrder*> idToOrderMap;
     std::map<Price, lob::core::PriceLevel, std::greater<Price>> bidLevels_;
     std::map<Price, lob::core::PriceLevel, std::less<Price>> askLevels_;
