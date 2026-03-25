44400179b74175db15242c4719d98969415e8019
Ethan Mathew <ethan.mathew55555@gmail.com>
2026-03-25 10:54:19 -0400

    fix: adjust simple limit order matching logic for branch efficiency and correctness

--- src/Orderbook.cpp
+++ src/Orderbook.cpp
@@ -59,57 +59,52 @@
     void OrderBook::submit_limit_order_resting(const LimitOrderRequest& limitRequest)
     {
         Quantity remainingShares = limitRequest.quantity_;
-
+        std::vector<OrderID> executedOrders;
+        
         if constexpr (S == Side::BUY)
         {
-            if (!topOfAsks.empty())
+            if (!askLevels_.empty())
             {
+                while (!askLevels_.empty() &&
+                       remainingShares > 0 &&
+                       crosses<Side::BUY>(limitRequest.price_, topOfAsks->first))
+                {
+                    auto topOfAsks = askLevels_.begin();
+                    core::PriceLevel& matchLevel = topOfAsks.second;
+                    core::RestingOrder* takingOrder = matchLevel.front();
+                    
+                    // If the first order in the level has enough quantity to fulfill the request
+                    if (takingOrder->quantity_ > remainingShares)
+                    {
+                        matchLevel.take_shares_from_first(remainingShares);
+                        remainingShares = 0;
+                    }
+                    else
+                    {
+                        remainingShares -= takingOrder->quantity_;
+                        matchLevel.take_all_shares_from_first();
+                        executedOrders.push_back(takingOrder->id_);
+                        //deallocate takingOrder
+                    }
 
-            }
-
-            auto topOfAsks = askLevels_.begin();
-
-            std::vector<RestingOrder*> executedOrders;
-
-            while (remainingShares > 0 &&
-                   !matchLevel.empty() &&
-                   !askLevels_.empty() &&
-                   crosses<Side::BUY>(limitRequest.price_, topOfAsks->first))
-            {
-                // execute on the price level
-                core::RestingOrder* takingOrder = matchLevel.front();
+                    if (matchLevel.empty())
+                    {
+                        askLevels_.erase(topOfAsks); // amortized O(1)
+                    }
+                }
                 
-                // If the first order in the level has enough quantity to fulfill the request
-                if (takingOrder->quantity_ >= remainingShares)
-                {
-                    matchLevel.take_shares(remainingShares);
-                    remainingShares = 0;
-                }
-                else
-                {
-                    remainingShares -= takingOrder->quantity_;
-                    matchLevel.take_all_shares();
-                }
-                
-                if (takingOrder->empty())
-                {
-                    executedOrders.push_back(matchLevel->pop_front());
-                }
-
-                if (matchLevel.empty())
-                {
-                    askLevels_.erase(topOfAsks->first);
-                    topOfAsks = askLevels_.begin();
-                }
-            }
-
-            if (remainingShares > 0)
-            {
                 // rest the remaining if there is remaining
-                // need to allocate on memory pool to create new order, then push back on the price level
+                // occurs when asks is empty or limit price doesn't cross
+                if (remainingShares > 0)
+                {
+                    
+                    // need to allocate on memory pool to create new order, then push back on the price level
+                }
+
+                // return execution result
             }
 
-            // return execution result
+
             
             askLevels_.emplace(limitRequest.price_, core::PriceLevel{limitRequest.price_});
 
