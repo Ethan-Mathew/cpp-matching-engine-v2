97643e67df3cdb5e66266e9e2f58c2c8208208c8
Ethan Mathew <ethan.mathew55555@gmail.com>
2026-03-25 21:04:52 -0400

    feat: implement internal execution result constructs

---
+++ src/core/InternalExecutionResults.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "Aliases.hpp"
+
+#include <cstdint>
+#include <vector>
+
+namespace lob
+{
+
+enum class SubmitStatus : std::uint8_t
+{
+    REJECTED,
+    PARTIALLY_FILLED,
+    CANCELLED_REMAINDER,
+    RESTING,
+    FILLED
+};
+
+enum class CancelResult : std::uint8_t
+{
+    NOT_FOUND,
+    CANCELLED
+};
+
+enum class ModificationResult : std::uint8_t
+{
+    NOT_FOUND,
+    MODIFIED
+};
+
+struct ExecutionResult
+{
+    OrderID touchedOrderID_;
+    Price touchedPrice;
+    Quantity touchedQuantity;
+};
+
+struct SubmissionResult
+{
+    Quantity quantityFilled_;
+    Quantity quantityRemaining_;
+    SubmitStatus status_;
+    std::vector<ExecutionResult> executions_;
+};
+
+} // namespace lob
