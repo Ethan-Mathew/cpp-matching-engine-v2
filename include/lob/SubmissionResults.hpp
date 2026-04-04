5651e075e74429f5fb635353824df4f715279d7d
Ethan Mathew <ethan.mathew55555@gmail.com>
2026-04-04 03:53:32 -0400

    fix: update ExecutionResult usage, eliminating redundant namespace qualifier and resolve circular dependency of includes

--- include/lob/SubmissionResults.hpp
+++ include/lob/SubmissionResults.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "Aliases.hpp"
-#include "OrderBook.hpp"
+#include "ExecutionResults.hpp"
 
 #include <vector>
 
@@ -22,7 +22,7 @@ struct SubmissionResult
     Quantity quantityRequested_ = 0;
     Quantity quantityFilled_    = 0;
     SubmitStatus status_;
-    std::vector<OrderBook::ExecutionResult> executions_;
+    std::vector<ExecutionResult> executions_;
 
     Quantity get_quantity_remaining() const
     {
