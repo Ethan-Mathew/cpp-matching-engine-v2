1d74ba3c12870da6d580d4e364583db9e13b992c
Ethan Mathew <ethan.mathew55555@gmail.com>
2026-04-06 22:50:26 -0400

    style: eliminate alias comments

--- include/lob/Aliases.hpp
+++ include/lob/Aliases.hpp
@@ -5,9 +5,9 @@
 namespace lob
 {
 
-using Quantity  = std::int32_t;     // Shares per order
-using Price     = std::int64_t;     // Using fixed-point arithmetic - multiplied by 10000
-using OrderID   = std::uint64_t;    // Unique order ID
-using Volume    = std::uint64_t;    // Shares outstanding
+using Quantity  = std::int32_t;
+using Price     = std::int64_t; // Using fixed-point arithmetic - multiplied by 10000
+using OrderID   = std::uint64_t;
+using Volume    = std::uint64_t;
 
 } // namespace lob
